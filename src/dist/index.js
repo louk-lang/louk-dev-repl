fc24274bfa8d74f42f999d3c3dc42e80e5d5be34
agorischek <alex.gorischek@gmail.com>
2018-11-17 22:25:15 -0800

    Remove embedded Louk library

--- src/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var parser = require("./parser");
-module.exports = function (content, options) {
-    return parser.parse(content, options);
-};
--- src/dist/utils.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function clone(input) {
-    return JSON.parse(JSON.stringify(input));
-}
-exports.clone = clone;
